27f205efd4f3e8349550d74439bda48e07432a99
Simon Brand <simon.brand@postadigitale.de>
2022-08-05 17:10:56 +0200

    Test decrypt_secure_boot_keys with actual tar file

--- tests/unit/decrypt.py
+++ tests/unit/decrypt.py
@@ -1,9 +1,12 @@
 import unittest
+from pathlib import Path
 from unittest import mock
 from unittest.mock import call
 from verify_squash_root.decrypt import format_cmd, TAR_FILE, KEY_DIR, \
     decrypt_secure_boot_keys, DecryptKeys
-from .test_helper import wrap_tempdir
+from .test_helper import wrap_tempdir, get_test_files_path
+
+MOCK_BASE = "verify_squash_root.decrypt"
 
 
 class DecryptTest(unittest.TestCase):
@@ -15,15 +18,14 @@ class DecryptTest(unittest.TestCase):
                           "/etc/keys.tar.age"])
 
     @wrap_tempdir
-    def test__decrypt_secure_boot_keys(self, tempdir):
-        base = "verify_squash_root.decrypt"
+    def test__decrypt_secure_boot_keys__mock(self, tempdir):
         key_dir = tempdir / "keys"
         all_mocks = mock.MagicMock()
-        with (mock.patch("{}.exec_binary".format(base),
+        with (mock.patch("{}.exec_binary".format(MOCK_BASE),
                          new=all_mocks.exec_binary),
-              mock.patch("{}.KEY_DIR".format(base),
+              mock.patch("{}.KEY_DIR".format(MOCK_BASE),
                          new=key_dir),
-              mock.patch("{}.tarfile".format(base),
+              mock.patch("{}.tarfile".format(MOCK_BASE),
                          new=all_mocks.tarfile)):
             config = {
                 "DEFAULT": {
@@ -41,13 +43,38 @@ class DecryptTest(unittest.TestCase):
                  call.tarfile.open().__exit__(None, None, None)])
             self.assertEqual(key_dir.is_dir(), True)
 
+    @wrap_tempdir
+    def test__decrypt_secure_boot_keys__filesystem(self, tempdir):
+        key_dir = tempdir / "keys"
+        tarpath = get_test_files_path("decrypt") / "keys.tar"
+        config = {
+            "DEFAULT": {
+                "DECRYPT_SECURE_BOOT_KEYS_CMD": "cp {} {{}}".format(tarpath),
+            }
+        }
+        self.assertEqual(TAR_FILE,
+                         Path("/tmp/verify_squash_root/keys/keys.tar"))
+        self.assertEqual(key_dir.is_dir(), False)
+        with (mock.patch("{}.TAR_FILE".format(MOCK_BASE),
+                         new=key_dir / "keys.tar"),
+              mock.patch("{}.KEY_DIR".format(MOCK_BASE),
+                         new=key_dir)):
+            decrypt_secure_boot_keys(config)
+        self.assertEqual(key_dir.is_dir(), True)
+        key = (b"Secret DB Key\ntest file\n"
+               b"From tar file\nIn Love\n - Maintainer\n")
+        self.assertEqual(
+            (key_dir / "db.key").read_bytes(), key)
+        self.assertEqual(
+            (key_dir / "db.crt").read_bytes(),
+            b"The DB cert file\nextracted\n")
+
     def test__decrypt_keys(self):
-        base = "verify_squash_root.decrypt"
         all_mocks = mock.Mock()
         config = mock.Mock()
-        with (mock.patch("{}.decrypt_secure_boot_keys".format(base),
+        with (mock.patch("{}.decrypt_secure_boot_keys".format(MOCK_BASE),
                          new=all_mocks.decrypt_secure_boot_keys),
-              mock.patch("{}.shutil".format(base),
+              mock.patch("{}.shutil".format(MOCK_BASE),
                          new=all_mocks.shutil)):
             with DecryptKeys(config):
                 all_mocks.inner_func()
